3b50678d132090285241df2e12d664c61a317239
gadf123 <gadf123@naver.com>
2018-02-07 20:08:20 +0900

    swagger 추가

---
+++ src/main/java/heresy/config/WebConfigration.java
@@ -0,0 +1,31 @@
+package heresy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @user updown
+ * @date 2018. 2. 7.
+ **/
+
+
+@Configuration
+public class WebConfigration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("PUT", "DELETE", "POST", "GET")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+    }
+
+}
